4f69ee222d58ef772f61ac273a2c9312d6ee8166
AlejaSD <salgadoalejandra09az@gmail.com>
2025-04-11 19:17:02 -0500

    project created

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Screens/Login";
+import Signup from "./Screens/Signup";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Login />} /> 
+          <Route path={"/registrarse"} element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
