e29037c5498b97a1f83384f41d0fa131f9f65f32
AlejaSD <salgadoalejandra09az@gmail.com>
2025-04-12 19:27:40 -0500

    project has structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Screens/Login";
-import Signup from "./Screens/Signup";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Login />} /> 
-          <Route path={"/registrarse"} element={<Signup />} />
+          
         </Routes>
       </BrowserRouter>
     </>
